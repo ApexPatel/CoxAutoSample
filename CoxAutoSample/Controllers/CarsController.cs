d11a233aa7a48ec409678e55cf636751336fdc80
ApexPatel <apex.patel86@gmail.com>
2019-08-30 02:34:31 -0400

    Add files via upload

---
+++ CoxAutoSample/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Data;
+using System.Text.RegularExpressions;
+using CoxAutoSample.Models;
+
+namespace CoxAutoSample.Controllers
+{
+    public class CarsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase FileUpload)
+        {
+            CarsModel objCarsModel = new CarsModel();
+            return View(objCarsModel.UplaodCarDetails(FileUpload));
+        }
+
+        
+
+    }
+}
